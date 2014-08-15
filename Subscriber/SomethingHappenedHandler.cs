3b41cd7ae229d0c22f7a5950abb2b74156955da3
lukemcgregor <luke@mcgregor.org.nz>
2014-08-15 14:56:12 +1200

    Errors

--- Subscriber/SomethingHappenedHandler.cs
+++ Subscriber/SomethingHappenedHandler.cs
@@ -9,6 +9,11 @@ namespace Subscriber
 		public void Handle(SomethingHappened message)
 		{
 			Console.WriteLine("Something happened event recieved with: " + message.SomeText);
+
+			if (message.SomeText.ToLower().Contains("bug"))
+			{
+				throw new Exception("EXPLODE!!!!");
+			}
 		}
 	}
 }
